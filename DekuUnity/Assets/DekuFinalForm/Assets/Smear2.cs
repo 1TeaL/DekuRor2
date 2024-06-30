6b0033ade0a60c2293739fd9ce1e8d559c5ff463
1TeaL <soxas.roxas@gmail.com>
2024-06-30 23:49:28 +1000

    smear shader, blender anims start

---
+++ DekuUnity/Assets/DekuFinalForm/Assets/Smear2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Smear2 : MonoBehaviour
+{
+    Queue<Vector3> _recentPositions = new Queue<Vector3>();
+    Vector3 motionVector;
+
+    [SerializeField]
+    int _frameLag = 0;
+    [SerializeField]
+    Transform targetTransform;
+
+
+    Material _smearMat = null;
+    public Material smearMat
+    {
+        get
+        {
+            if (!_smearMat)
+                _smearMat = GetComponent<Renderer>().material;
+            return _smearMat;
+        }
+    }
+
+    void Start()
+    {
+        _smearMat = GetComponent<Renderer>().material;
+    }
+
+
+    void LateUpdate()
+    {
+
+        if (_recentPositions.Count > _frameLag)
+        {
+            motionVector = targetTransform.position - _recentPositions.Dequeue();
+            smearMat.SetVector("_SmearDirection", motionVector);
+        }
+
+
+        _recentPositions.Enqueue(targetTransform.position);
+    }
+
+}
